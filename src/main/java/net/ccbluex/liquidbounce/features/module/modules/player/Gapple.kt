190e4b4b5faa3f0d993c29850e9e78828dbaaf59
jiuxianqwq <3881298251@qq.com>
2025-02-12 03:16:55 +0800

    Gapple触发血量

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/Gapple.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/Gapple.kt
@@ -27,7 +27,7 @@ import net.minecraft.util.ResourceLocation
 import net.minecraftforge.fml.common.Mod.EventHandler
 
 object Gapple : Module("Gapple",Category.PLAYER) {
-//    private val heal by int("health", 20, 0..40)
+    private val heal by int("health", 20, 0..40)
     private val sendDelay by int("SendDelay",3,1..10)
     private val sendOnceTicks = 1;
     private val stuck by boolean("Stuck",false)
@@ -75,96 +75,109 @@ object Gapple : Module("Gapple",Category.PLAYER) {
     }
 
     val onTick = handler<PreTickEvent> {
-        if (!eating) {
-            target = KillAura.target
-        }
-
-        if (MinecraftInstance.mc.thePlayer == null || MinecraftInstance.mc.thePlayer.isDead) {
-            BlinkUtils.stopBlink()
-            state = false
-            return@handler
-        }
-        if (slot == -1) {
-            state = false
-            return@handler
-        }
-        if (eating) {
-            if (stuck) {
-                StuckUtils.stuck()
+        if(mc.thePlayer.health < heal) {
+            if (!eating) {
+                target = KillAura.target
             }
-            if (!BlinkUtils.blinking) {
-                BlinkUtils.blink(
-                    C09PacketHeldItemChange::class.java,
-                    C0EPacketClickWindow::class.java,
-                    C0DPacketCloseWindow::class.java
-                )
-                BlinkUtils.setCancelReturnPredicate(C07PacketPlayerDigging::class.java) { it -> (it as C07PacketPlayerDigging).status == C07PacketPlayerDigging.Action.RELEASE_USE_ITEM }
-                BlinkUtils.setCancelReturnPredicate(C08PacketPlayerBlockPlacement::class.java) { it ->
-                    ((it as C08PacketPlayerBlockPlacement).position.y == -1)
+
+            if (MinecraftInstance.mc.thePlayer == null || MinecraftInstance.mc.thePlayer.isDead) {
+                BlinkUtils.stopBlink()
+                state = false
+                return@handler
+            }
+            if (slot == -1) {
+                state = false
+                return@handler
+            }
+            if (eating) {
+                if (stuck) {
+                    StuckUtils.stuck()
                 }
-                BlinkUtils.setCancelReturnPredicate(C02PacketUseEntity::class.java) { it -> noCancelC02 }
-                BlinkUtils.setCancelReturnPredicate(C0APacketAnimation::class.java) { it -> noCancelC02 }
-                BlinkUtils.setCancelAction(C03PacketPlayer::class.java) { packet -> c03s++ }
-                BlinkUtils.setReleaseAction(C03PacketPlayer::class.java) { packet -> c03s-- }
-                BlinkUtils.setReleaseReturnPredicateMap(C02PacketUseEntity::class.java) { packet -> !eating && noC02 }
-                BlinkUtils.setCancelAction(C02PacketUseEntity::class.java) { packet -> c02s++ }
-                BlinkUtils.setReleaseAction(C02PacketUseEntity::class.java) { packet -> c02s-- }
-                canStart = true
-            }
-        } else {
-            eating = true
-            chat("开始吃")
-        }
-        if (c03s >= 32) {
-            eating = false
-            pulsing = true
-            BlinkUtils.resetBlackList()
-            sendPacket(C09PacketHeldItemChange(slot), false)
-            println("Start!")
-            sendPacket(
-                C08PacketPlayerBlockPlacement(
-                    MinecraftInstance.mc.thePlayer.inventoryContainer.getSlot(
-                        slot + 36
-                    ).stack
-                ), false
-            )
-            if (ViaLoadingBase.getInstance().targetVersion.newerThanOrEqualTo(ProtocolVersion.v1_12_2)) {
-                sendPacket(
-                    C08PacketPlayerBlockPlacement(BlockPos(-1, -2, -1), 255, null, 0.0f, 0.0f, 0.0f)
-                    , false
-                )
-            }
-            BlinkUtils.stopBlink()
-            println("Stop!")
-            chat("吃完了")
-            sendPacket(C09PacketHeldItemChange(MinecraftInstance.mc.thePlayer.inventory.currentItem), false)
-            pulsing = false
-            if (autoGapple) {
-                c03s = 0
-
-                slot = InventoryUtils.findItem(36, 45, Items.golden_apple) ?: -1;
-
-                if (slot != -1) {
-                    slot = slot - 36
+                if (!BlinkUtils.blinking) {
+                    BlinkUtils.blink(
+                        C09PacketHeldItemChange::class.java,
+                        C0EPacketClickWindow::class.java,
+                        C0DPacketCloseWindow::class.java
+                    )
+                    BlinkUtils.setCancelReturnPredicate(C07PacketPlayerDigging::class.java) { it -> (it as C07PacketPlayerDigging).status == C07PacketPlayerDigging.Action.RELEASE_USE_ITEM }
+                    BlinkUtils.setCancelReturnPredicate(C08PacketPlayerBlockPlacement::class.java) { it ->
+                        ((it as C08PacketPlayerBlockPlacement).position.y == -1)
+                    }
+                    BlinkUtils.setCancelReturnPredicate(C02PacketUseEntity::class.java) { it -> noCancelC02 }
+                    BlinkUtils.setCancelReturnPredicate(C0APacketAnimation::class.java) { it -> noCancelC02 }
+                    BlinkUtils.setCancelAction(C03PacketPlayer::class.java) { packet -> c03s++ }
+                    BlinkUtils.setReleaseAction(C03PacketPlayer::class.java) { packet -> c03s-- }
+                    BlinkUtils.setReleaseReturnPredicateMap(C02PacketUseEntity::class.java) { packet -> !eating && noC02 }
+                    BlinkUtils.setCancelAction(C02PacketUseEntity::class.java) { packet -> c02s++ }
+                    BlinkUtils.setReleaseAction(C02PacketUseEntity::class.java) { packet -> c02s-- }
+                    canStart = true
                 }
             } else {
-                state = false
+                eating = true
+                chat("开始吃")
             }
-            if (autoBlock != "Off" && (!blinked || !net.ccbluex.liquidbounce.utils.client.BlinkUtils.isBlinking) && target != null) {
-                if(autoBlock == "QuickMarco"){
-                    sendOffHandUseItem()
-                }else if (autoBlock == "Packet") {
-                    sendPacket(C08PacketPlayerBlockPlacement(mc.thePlayer.heldItem))
+            if (c03s >= 32) {
+                eating = false
+                pulsing = true
+                BlinkUtils.resetBlackList()
+                sendPacket(C09PacketHeldItemChange(slot), false)
+                println("Start!")
+                sendPacket(
+                    C08PacketPlayerBlockPlacement(
+                        MinecraftInstance.mc.thePlayer.inventoryContainer.getSlot(
+                            slot + 36
+                        ).stack
+                    ), false
+                )
+                if (ViaLoadingBase.getInstance().targetVersion.newerThanOrEqualTo(ProtocolVersion.v1_12_2)) {
+                    sendPacket(
+                        C08PacketPlayerBlockPlacement(BlockPos(-1, -2, -1), 255, null, 0.0f, 0.0f, 0.0f), false
+                    )
                 }
-                slotChangeAutoBlock = false
-                chat("发送防砍包")
-            }
-            return@handler
-        }
+                BlinkUtils.stopBlink()
+                println("Stop!")
+                chat("吃完了")
+                sendPacket(C09PacketHeldItemChange(MinecraftInstance.mc.thePlayer.inventory.currentItem), false)
+                pulsing = false
+                if (autoGapple) {
+                    c03s = 0
 
-        if ((MinecraftInstance.mc.thePlayer.ticksExisted % sendDelay) == 0) {
-            for (i in 0..<sendOnceTicks) {
-                BlinkUtils.releasePacket(true)
+                    slot = InventoryUtils.findItem(36, 45, Items.golden_apple) ?: -1;
+
+                    if (slot != -1) {
+                        slot = slot - 36
+                    }
+                } else {
+                    state = false
+                }
+                if (autoBlock != "Off" && (!blinked || !net.ccbluex.liquidbounce.utils.client.BlinkUtils.isBlinking) && target != null) {
+                    if (autoBlock == "QuickMarco") {
+                        sendOffHandUseItem()
+                    } else if (autoBlock == "Packet") {
+                        sendPacket(C08PacketPlayerBlockPlacement(mc.thePlayer.heldItem))
+                    }
+                    slotChangeAutoBlock = false
+                    chat("发送防砍包")
+                }
+                return@handler
+            }
+
+            if ((MinecraftInstance.mc.thePlayer.ticksExisted % sendDelay) == 0) {
+                for (i in 0..<sendOnceTicks) {
+                    BlinkUtils.releasePacket(true)
+                }
+            }
+        }else{
+            eating = false
+
+            if (canStart) {
+                pulsing = false
+                eating = false
+                BlinkUtils.stopBlink()
+            }
+
+            if (stuck) {
+                StuckUtils.stopStuck()
             }
         }
     }
